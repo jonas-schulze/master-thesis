d0217895ba8e1fef0d663bd7698c34a734a7a1aa
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2021-06-09 14:00:24 +0200

    Store traces in HDF5 containier

--- dense-dre-matlab/LOGBOOK.md
+++ dense-dre-matlab/LOGBOOK.md
@@ -1,6 +1,6 @@
 # TODO
 
-- [ ] Update Matlab demo script: store K, X traces in HDF5 container
+- [x] Update Matlab demo script: store K, X traces in HDF5 container
 - [ ] Add Julia script that stores K, X traces in HDF5 container
 - [ ] Write jobfile(s)
 - [ ] Compare Matlab and Julia results
--- dense-dre-matlab/demo_rail_euler.m
+++ dense-dre-matlab/demo_rail_euler.m
@@ -1,16 +1,25 @@
 %% Setup
 P = load('Rail371.mat');
 
-tspan = 0:25:900;
+dt = 300
+tspan = 0:dt:900;
 
 %% Solve
-%[Kc,Xc] = g_dense_rb1o(P.E, P.A, P.B, P.C, P.X0, tspan);
+[Kc,Xc] = g_dense_rb1o(P.E, P.A, P.B, P.C, P.X0, tspan);
 %[Kc,Xc] = g_dense_rb2o(P.E, P.A, P.B, P.C, P.X0, tspan);
 %[Kc,Xc] = g_dense_rb3o(P.E, P.A, P.B, P.C, P.X0, tspan);
-[Kc,Xc] = g_dense_rb4o(P.E, P.A, P.B, P.C, P.X0, tspan);
+%[Kc,Xc] = g_dense_rb4o(P.E, P.A, P.B, P.C, P.X0, tspan);
 
-%% Plot 
-Ka = cell2mat(Kc');
-plot(tspan, Ka(1:7:end, 71));
-xlabel('t');
-ylabel('K_{1,71}');
+%% Store
+container = "baseline" + dt + ".h5"
+for i = 1:length(tspan)
+  t = tspan(i)
+  K = cell2mat(Kc(i))
+  X = cell2mat(Xc(i))
+  K_path = "/K/t=" + t
+  X_path = "/X/t=" + t
+  h5create(container, K_path, size(K))
+  h5create(container, X_path, size(X))
+  h5write(container, K_path, K)
+  h5write(container, X_path, X)
+end
