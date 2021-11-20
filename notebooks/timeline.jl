aaf4284fb8e5ad75fbd2a9f6b86fe97078297c55
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2021-11-20 19:06:53 +0100

    Update timeline notebook

--- notebooks/timeline.jl
+++ notebooks/timeline.jl
@@ -15,33 +15,41 @@ macro bind(def, element)
 end
 
 # ╔═╡ 3dfd9d2e-89fc-4cba-9942-f7cf2142be2b
-using DrWatson, DataFrames, CairoMakie, PlutoUI
+using DrWatson
 
 # ╔═╡ fe6218a8-d31f-47ba-948c-819ea42d379f
 @quickactivate
 
+# ╔═╡ 8f401775-95cd-4d7e-9318-5f3908edf37e
+using DataFrames, CairoMakie, PlutoUI, UnPack
+
 # ╔═╡ 2e393b06-46f5-11ec-3258-8371b50807e7
-md"""
-# Parareal Timeline
+md"# Parareal Timeline"
 
-Select the `dataset` for which to visualize the timeline:
-"""
-
-# ╔═╡ 0c54e190-8dab-4021-8b89-88299c2f07af
+# ╔═╡ 09b6dd8a-15a6-4ad0-8582-e859a4e693bf
 let
 	global dataset
 	dirs = readdir(datadir("dense-par"))
 	@bind dataset Select(dirs; default=first(dirs))
 end
 
+# ╔═╡ 961530f7-61e7-410c-b7b4-7dbdacb6088e
+@bind nevent Select(1:10, default=1)
+
 # ╔═╡ d95a40fd-7d48-420c-b49e-e40e721639a6
 md"""
 The above plot shows the time a worker process (stage) started, each time it received a new initial value to compute a single iteration with, and when it finished.
 """
 
+# ╔═╡ 390b6d88-bf4a-41e8-ad9d-d44c14abb681
+md"## Waiting times"
+
 # ╔═╡ 536adf23-691d-49be-8656-8340f3089582
 md"""
 ## Flat leading slope
+
+The discussion below applies only to cases without JIT warm-up,
+i.e. `wc == wf == false`.
 
 Note the flat temporal slope of the first "Running" dots,
 which is caused by the (JIT) compilation time of each individual process.
@@ -124,25 +132,96 @@ begin
 	df
 end
 
+# ╔═╡ 74a288c3-f5c2-4e41-aef0-6d44c8d3628b
+@bind nstage Select(1:maximum(df.stage), default=1)
+
+# ╔═╡ 0fc4017b-318a-43ce-89cd-b33ee53e0286
+combine(groupby(df, :stage)) do g
+	wid = findall(==("Waiting"), g.status)
+	rid = wid .+ 1
+	@assert all(in(("Running", "DoneWaiting")), g.status[rid])
+	wtime = g.time_sent[wid]
+	rtime = g.time_sent[rid]
+	Ref(rtime - wtime)
+end
+
+# ╔═╡ 42b6371c-c1a9-45c2-8a27-98a47d8d0c32
+states = unique(df.status)
+
+# ╔═╡ 2f487c91-3b22-4941-a309-eafc3ce295f1
+@bind event Select(states, default="Running" in states ? "Running" : "DoneWaiting")
+
+# ╔═╡ 5b208c16-5a6a-4067-a0e4-b68c6aa1aade
+md"""
+## Linear Approximation of "$event"s
+
+Find the best ``t \mapsto \alpha + \beta \cdot t \approx \text{stage}``:
+"""
+
+# ╔═╡ b838b465-42cd-41fa-9751-c8b0cd93f345
+let
+	global slope
+	events = filter(:status => ==(event), df)
+	filter!(:stage => >=(nstage), events)
+	slope = combine(
+		groupby(events, :stage),
+		:time_sent => (t -> get(t, nevent, missing)) => :time,
+	)
+	dropmissing!(slope)
+	slope
+end
+
+# ╔═╡ 18aeb876-5106-4ef2-a091-8e3c3c2c0836
+let
+	global α, β
+	x = slope.time
+	y = slope.stage
+	A = [ones(length(x)) x]
+	α, β = A \ y
+end
+
+# ╔═╡ ba95bb72-57d7-41a5-94e4-2ba6d2c9fd3d
+md"""
+The `nevent`-th occurences of `event` starting from stage `nstage` evolve at
+~$(round(1/β, digits=3)) seconds per stage.
+"""
+
 # ╔═╡ edfbd427-6d01-45f6-ae10-d309fe2723a7
 let
+	@unpack nc, nf, oc, of = config
+	wc = get(config, "wc", false)
+	wf = get(config, "wf", false)
+	tconf = @strdict wc wf nc nf oc of
+	tdesc = savename(tconf, connector=", ")
+
 	f = Figure()
 	ax = Axis(
 		f[1, 1],
-		title = "Timeline ($model, $desc)",
+		title = """
+		Timeline ($model, $tdesc)
+		$(round(1/β, digits=3)) seconds per stage
+		""",
 		ylabel = "stage",
 		xticks = 0:60:maximum(df.time_sent),
 		xtickformat = xs -> ["$(round(Int, x÷60))min" for x in xs],
 	)
 	hideydecorations!(ax, ticklabels=false, label=false)
-	
-	_started = df.status .== "Started"
-	_running = df.status .== "Running"
-	_done = df.status .== "Done"
-	
-	scatter!(df[_started, :time_sent], df[_started, :stage]; label="Started")
-	scatter!(df[_running, :time_sent], df[_running, :stage]; label="Running")
-	scatter!(df[_done, :time_sent], df[_done, :stage]; label="Done")
+
+	names = Dict(
+		"DoneWaiting" => "Running",
+	)
+
+	for s in ["Started", "Running", "DoneWaiting", "Done"]
+		s in states || continue
+		selection = df.status .== s
+		scatter!(
+			df[selection, :time_sent],
+			df[selection, :stage];
+			label=get(names, s, s),
+		)
+	end
+
+	abline!(ax, α, β)
 
 	Legend(f[1, 2], ax)
 
@@ -151,15 +230,26 @@ end
 
 # ╔═╡ Cell order:
 # ╟─2e393b06-46f5-11ec-3258-8371b50807e7
-# ╟─0c54e190-8dab-4021-8b89-88299c2f07af
+# ╠═09b6dd8a-15a6-4ad0-8582-e859a4e693bf
+# ╟─ba95bb72-57d7-41a5-94e4-2ba6d2c9fd3d
+# ╠═2f487c91-3b22-4941-a309-eafc3ce295f1
+# ╠═961530f7-61e7-410c-b7b4-7dbdacb6088e
+# ╠═74a288c3-f5c2-4e41-aef0-6d44c8d3628b
 # ╟─edfbd427-6d01-45f6-ae10-d309fe2723a7
 # ╟─d95a40fd-7d48-420c-b49e-e40e721639a6
+# ╟─5b208c16-5a6a-4067-a0e4-b68c6aa1aade
+# ╠═b838b465-42cd-41fa-9751-c8b0cd93f345
+# ╠═18aeb876-5106-4ef2-a091-8e3c3c2c0836
+# ╟─390b6d88-bf4a-41e8-ad9d-d44c14abb681
+# ╠═0fc4017b-318a-43ce-89cd-b33ee53e0286
 # ╟─536adf23-691d-49be-8656-8340f3089582
 # ╟─1aed787b-9793-428e-95b3-e7c8b2695f05
 # ╟─88630af8-3193-4487-871d-cce8e93985d4
 # ╠═b2df1ca8-ff2d-4f63-a484-a59b84dbcd82
 # ╠═4cabaffe-99fa-4d7e-b8e9-34afb0830fa3
+# ╠═42b6371c-c1a9-45c2-8a27-98a47d8d0c32
 # ╠═9c21b470-ae46-42d2-a192-d7e3691dd7a8
 # ╠═3783109b-7db2-4b1a-8fcc-459b22791fe2
 # ╠═3dfd9d2e-89fc-4cba-9942-f7cf2142be2b
 # ╠═fe6218a8-d31f-47ba-948c-819ea42d379f
+# ╠═8f401775-95cd-4d7e-9318-5f3908edf37e
