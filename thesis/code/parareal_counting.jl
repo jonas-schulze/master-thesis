9e44c40d5fd4f42c9c1da31998327813618bc7c1
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2022-03-01 13:43:05 +0100

    Update remarks on implementation of ParaReal.jl

--- thesis/code/parareal_counting.jl
+++ thesis/code/parareal_counting.jl
@@ -1,10 +1,12 @@
 using ParaReal
 using Distributed: addprocs, @everywhere
 
-addprocs(3)
+N = 3
+addprocs(N)
 @everywhere using ParaReal
 
 # Define problem type:
+#== problem ==#
 @everywhere begin
     struct SomeProblem
         v
@@ -13,31 +15,50 @@ addprocs(3)
     ParaReal.initial_value(p::SomeProblem) = p.v
     ParaReal.remake_prob(::SomeProblem, v, tspan) = SomeProblem(v, tspan)
 end
+#== end ==#
 
 # Define solution type:
+#== solution ==#
 @everywhere begin
     struct Counters
         F::Int
         G::Int
     end
-    ParaReal.value(c::Counters) = c
+    ParaReal.value(c::Counters) = c #* \label{line:impl:parareal_counting:value}
 
     # Needed by default parareal update implementation:
     Base.:(+)(c1::Counters, c2::Counters) = Counters(c1.F + c2.F, c1.G + c2.G)
     Base.:(-)(c::Counters) = c
 end
+#== end ==#
 
-# Define solvers:
+# Instantiate solvers, problem, and compute solution:
+#== solve ==#
 inc_F = (p::SomeProblem) -> Counters(p.v.F + 1, p.v.G)
 inc_G = (p::SomeProblem) -> Counters(p.v.F, p.v.G + 1)
 
-# Instantiate problem, solvers, and compute solution:
-tspan = (0., 42.)
-prob = ParaReal.Problem(SomeProblem(Counters(0, 0), tspan))
-alg = ParaReal.Algorithm(inc_G, inc_F)
+tspan = (0., 42.) # doesn't matter here
+prob = SomeProblem(Counters(0, 0), tspan)
 sol = solve(
-    prob, alg;
+    ParaReal.Problem(prob),
+    ParaReal.Algorithm(inc_G, inc_F);
     maxiters=2,
-    nconverged=typemax(Int), # disable convergence checks, which call norm(::Counters)
-    rtol=0.0, # don't evaluate default, which calls size(::Counters, 1)
+    # disable convergence checks, which call norm(::Counters)
+    nconverged=typemax(Int),
+    # don't evaluate default rtol, which calls size(::Counters, 1)
+    rtol=0.0,
 )
+#== end ==#
+
+#== result ==#
+ParaReal.value(sol.stages[1])
+ParaReal.value(sol.stages[2])
+ParaReal.value(sol.stages[3])
+#== end ==#
+
+using Test
+using ParaReal: value
+
+@test value(sol.stages[1]) == Counters(1, 0)
+@test value(sol.stages[2]) == Counters(2, 0)
+@test value(sol.stages[3]) == Counters(7, 10)
