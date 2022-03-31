dc58b56ad4ad9138bebc6bbee906abf27c222bc1
Jonas Schulze <jschulze@mpi-magdeburg.mpg.de>
2022-03-31 18:05:01 +0200

    Use fewer cores for sequential solve

--- scripts/fig:results:sequential:rail.sh
+++ scripts/fig:results:sequential:rail.sh
@@ -1,6 +1,6 @@
 export MY_N=450
 
-MY_KIND=dense MY_O=1 sbatch -J lr1 seq.job
-MY_KIND=dense MY_O=2 sbatch -J lr2 seq.job
-MY_KIND=lowrank MY_O=1 sbatch -J de1 seq.job
-MY_KIND=lowrank MY_O=2 sbatch -J de2 seq.job
+MY_KIND=dense MY_O=1 sbatch -N1 -c1 -J lr1 seq.job
+MY_KIND=dense MY_O=2 sbatch -N1 -c1 -J lr2 seq.job
+MY_KIND=lowrank MY_O=1 sbatch -N1 -c1 -J de1 seq.job
+MY_KIND=lowrank MY_O=2 sbatch -N1 -c1 -J de2 seq.job
